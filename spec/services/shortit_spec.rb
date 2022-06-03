906413a6259ced6096801714fa0b0fd930aab2a2
Kaur <bjit_kaur@hotmail.com>
2022-06-03 13:14:18 +0100

    Tests for URL link model to check for the presence of URLs and uniqueness of seven char string output.

--- spec/services/shortit_spec.rb
+++ spec/services/shortit_spec.rb
@@ -2,6 +2,7 @@ require "rails_helper"
 
 RSpec.describe Shortit do 
 
+    # Test 1
     it "shorten a URL to a 7 Character String" do 
         # Original URL to be shorten
         orginal_url = "https://rubygems.org/gems/rspec/versions/3.5.0"
@@ -11,6 +12,7 @@ RSpec.describe Shortit do
         expect(shortit.seven_char_string.length).to eq(7)
     end
 
+    # Test 2
     # Ensure the Seven Char String of two URls are not equal
     it "gives URL its own seven character string" do 
         orginal_url = "https://www.amazon.co.uk/%E9%80%9A%E7%94%A8-Standing-Clothes-Jackets-Umbrellas/dp/B09QLSGLVJ/ref=sr_1_22?crid=14T2PEZTB7HI4&keywords=coat+stand&qid=1654185626&sprefix=coat+stand%2Caps%2C429&sr=8-22"
@@ -24,6 +26,7 @@ RSpec.describe Shortit do
         expect(string_one).not_to eq(string_two)
     end
 
+    # Test 3
     it "always gives same URL the same seven character string" do 
         original_url = "https://www.amazon.co.uk/%E9%80%9A%E7%94%A8-Standing-Clothes-Jackets-Umbrellas/dp/B09QLSGLVJ/ref=sr_1_22?crid=14T2PEZTB7HI4&keywords=coat+stand&qid=1654185626&sprefix=coat+stand%2Caps%2C429&sr=8-22"
         shortit = Shortit.new(original_url)
