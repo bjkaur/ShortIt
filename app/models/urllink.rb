583ebd8533ccfdc81c383eff26b8797dcb068bec
Kaur <bjit_kaur@hotmail.com>
2022-06-03 13:11:48 +0100

    Tests for URL link model to check for the presence of URLs and uniqueness of seven char string output.

--- app/models/urllink.rb
+++ app/models/urllink.rb
@@ -1,2 +1,6 @@
 class Urllink < ApplicationRecord
+    # Checks if seven_char_string and original url exists
+    # Checks if seven character string output is unique
+    validates_presence_of :seven_char_string, :original_url
+    validates_uniqueness_of :seven_char_string
 end
