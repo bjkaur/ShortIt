aea0d25bf899f9b8df5fd6e1adac1de225a9f300
Kaur <bjit_kaur@hotmail.com>
2022-06-03 15:17:27 +0100

    Test to generate unique code. Using dependency injection to use a Model. A loop to generate a seven char code. New method to generate short link using URL and seven char code. Private method to generate generate the seven char code.

--- spec/services/shortit_spec.rb
+++ spec/services/shortit_spec.rb
@@ -2,6 +2,7 @@ require "rails_helper"
 
 RSpec.describe Shortit do 
 
+    # Test 1
     it "shorten a URL to a 7 Character String" do 
         # Original URL to be shorten
         orginal_url = "https://rubygems.org/gems/rspec/versions/3.5.0"
@@ -11,6 +12,7 @@ RSpec.describe Shortit do
         expect(shortit.seven_char_string.length).to eq(7)
     end
 
+    # Test 2
     # Ensure the Seven Char String of two URls are not equal
     it "gives URL its own seven character string" do 
         orginal_url = "https://www.amazon.co.uk/%E9%80%9A%E7%94%A8-Standing-Clothes-Jackets-Umbrellas/dp/B09QLSGLVJ/ref=sr_1_22?crid=14T2PEZTB7HI4&keywords=coat+stand&qid=1654185626&sprefix=coat+stand%2Caps%2C429&sr=8-22"
@@ -24,16 +26,16 @@ RSpec.describe Shortit do
         expect(string_one).not_to eq(string_two)
     end
 
-    it "always gives same URL the same seven character string" do 
-        original_url = "https://www.amazon.co.uk/%E9%80%9A%E7%94%A8-Standing-Clothes-Jackets-Umbrellas/dp/B09QLSGLVJ/ref=sr_1_22?crid=14T2PEZTB7HI4&keywords=coat+stand&qid=1654185626&sprefix=coat+stand%2Caps%2C429&sr=8-22"
-        shortit = Shortit.new(original_url)
-        string_one = shortit.seven_char_string
+    # Test 3
+    # To generate unique output
+    it "creates a URL with a seven character string" do
+        url_link = "https://www.amazon.co.uk/%E9%80%9A%E7%94%A8-Standing-Clothes-Jackets-Umbrellas/dp/B09QLSGLVJ/ref=sr_1_22?crid=14T2PEZTB7HI4&keywords=coat+stand&qid=1654185626&sprefix=coat+stand%2Caps%2C429&sr=8-22"
+        shortit = Shortit.new(url_link)
+        short_url = shortit.generate_short_link
+        expect(short_url).to be_valid
 
-        original_url = "https://www.amazon.co.uk/%E9%80%9A%E7%94%A8-Standing-Clothes-Jackets-Umbrellas/dp/B09QLSGLVJ/ref=sr_1_22?crid=14T2PEZTB7HI4&keywords=coat+stand&qid=1654185626&sprefix=coat+stand%2Caps%2C429&sr=8-22"
-        shortit = Shortit.new(original_url)
-        string_two = shortit.seven_char_string
-
-        expect(string_one).to eq(string_two)
+        short_url2 = shortit.generate_short_link
+        expect(short_url2).to be_valid
     end
 
 end
