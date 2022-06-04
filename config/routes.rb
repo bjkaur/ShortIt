5c79b395d66cb108f0e982f33cdea3517161af57
Kaur <bjit_kaur@hotmail.com>
2022-06-04 15:26:53 +0100

    schema

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  #get 'links/create'
+  get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  post "/urllinks" => 'urllinks#create'
+  post 'urllinks', to: 'urllinks#create'
 end
