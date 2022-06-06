f02e6738a3cb2152bd59df5976a1146d3ff74f88
Kaur <bjit_kaur@hotmail.com>
2022-06-06 23:10:51 +0100

    original_url_format

--- app/models/urllink.rb
+++ app/models/urllink.rb
@@ -3,4 +3,16 @@ class Urllink < ApplicationRecord
     # Checks if seven character string output is unique
     validates_presence_of :seven_char_string, :original_url
     validates_uniqueness_of :seven_char_string
+
+    #Test: Model/ it is INVALID if it URL is NOT formatted suitabily
+    validate :original_url_format
+
+    def original_url_format
+    # Original url or empty string
+        uri = URI.parse(original_url || "")
+        if uri.host.nil?
+            # Errors that add original url
+            errors.add(:original_url, "Invalid URL format")
+        end
+    end
 end
